549261e6b13de9b088d5a03f2ac19c32ea0aa06c
Aiman Osama <aimanosama12345@gmail.com>
2025-12-08 17:33:44 +0200

    Update CIFAR_10_Deploy.py

--- CIFAR_10_Deploy.py
+++ CIFAR_10_Deploy.py
@@ -3,31 +3,49 @@ import streamlit as st
 import keras
 import io
 from PIL import Image
+import requests
 
-class_names = {'Airplane': '✈️','Horse': '🐴','Truck': '🚚','Automobile': '🚗','Ship': '🚢','Dog': '🐶','Bird': '🐦','Frog': '🐸','Cat': '🐱','Deer': '🦌'}
+class_names = {'Airplane': '✈️','Horse': '🐴','Truck': '🚚','Car': '🚗','Ship': '🚢','Dog': '🐶','Bird': '🐦','Frog': '🐸','Cat': '🐱','Deer': '🦌'}
 model = keras.models.load_model("model/CIFAR-10.keras")
 
 st.set_page_config(page_title="CIFAR_10_Classification", layout="centered")
 st.title("CIFAR_10_Classification")
-
 st.subheader("Available Classes")
+
 classes_line = "|".join([f"{emoji} **{name}**" for name, emoji in class_names.items()])
 st.markdown(classes_line)
 
 uploaded_file = st.file_uploader("Choose an image...")
+url_input = st.text_input("Or enter an image URL")
+
+image_data = None
 
 if uploaded_file is not None:
     image_data = uploaded_file.read()
+
+elif url_input:
+    try:
+        response = requests.get(url_input)
+        if response.status_code == 200:
+            image_data = response.content
+        else:
+            st.error("Could not load image from URL")
+    except Exception as e:
+        st.error(f"Invalid URL: {e}")
+
+if image_data:
+
     image_dis = Image.open(io.BytesIO(image_data))
-    image_dis = np.array([image_dis.resize((500, 500))])
+    image_show = np.array([image_dis.resize((500, 500))])
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
-        st.image(image_dis, caption="Uploaded Image")
+        st.image(image_show, caption="Input Image")
+
     pil_image = Image.open(io.BytesIO(image_data))
-    image_resized = np.array([pil_image.resize((32, 32))])
-    image_resized = image_resized/255
+    image_resized = np.array([pil_image.resize((32, 32))]) / 255
     result = model.predict(image_resized)
     pred = np.argmax(result)
+
     class_list = list(class_names.keys())
     predicted_name = class_list[pred]
     predicted_emoji = class_names[predicted_name]
