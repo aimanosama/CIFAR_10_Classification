72ed6b32a18773f8a27364093d1085cce1783b54
Aiman Osama <aimanosama12345@gmail.com>
2025-12-08 15:18:12 +0200

    Update CIFAR_10_Deploy.py

--- CIFAR_10_Deploy.py
+++ CIFAR_10_Deploy.py
@@ -4,11 +4,15 @@ import keras
 import io
 from PIL import Image
 
-class_names = ['Airplane','Horse','Truck','Automobile','Ship','Dog','Bird','Frog','Cat','Deer']
+class_names = {'Airplane': '✈️','Horse': '🐴','Truck': '🚚','Automobile': '🚗','Ship': '🚢','Dog': '🐶','Bird': '🐦','Frog': '🐸','Cat': '🐱','Deer': '🦌'}
 model = keras.models.load_model("model/CIFAR-10.keras")
 
 st.set_page_config(page_title="CIFAR_10_Classification", layout="centered")
 st.title("CIFAR_10_Classification")
+
+st.subheader("Available Classes")
+classes_line = "|".join([f"{emoji} **{name}**" for name, emoji in class_names.items()])
+st.markdown(classes_line)
 
 uploaded_file = st.file_uploader("Choose an image...")
 
@@ -23,5 +27,8 @@ if uploaded_file is not None:
     image_resized = np.array([pil_image.resize((32, 32))])
     image_resized = image_resized/255
     result = model.predict(image_resized)
-    pred = np.argmax(result, axis=1)
-    st.write("<center>" + class_names[pred[0]] + "</center>", unsafe_allow_html=True)
+    pred = np.argmax(result)
+    class_list = list(class_names.keys())
+    predicted_name = class_list[pred]
+    predicted_emoji = class_names[predicted_name]
+    st.markdown(f"<h3 style='text-align:center;'>Prediction: {predicted_emoji} {predicted_name}</h3>",unsafe_allow_html=True)
